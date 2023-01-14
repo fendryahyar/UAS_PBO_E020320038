56681be772d575730d244238374f983d26f5b9c6
fendryahyar <122244894+fendryahyar@users.noreply.github.com>
2023-01-14 21:39:12 +0800

    UAS_PBO_E020320038

---
+++ UAS_PBO_E020320038/FDA_E020320038/app/src/main/java/id/ac/poliban/mi/vb/e020320038/fdae020320038/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.vb.e020320038.fdae020320038.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ UAS_PBO_E020320038/Project82_E020320038/app/src/main/java/id/ac/poliban/mi/vb/e020320038/project82e020320038/MainActivity.java
@@ -0,0 +1,15 @@
+package id.ac.poliban.mi.vb.e020320038.project82e020320038;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        setTitle("Fendri - Project 82 E020320038");
+    }
+}
